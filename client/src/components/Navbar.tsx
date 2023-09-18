8427965d0c3f4d9828536cda769d3c613da4cfb4
JohnFuhrm12 <61069716+JohnFuhrm12@users.noreply.github.com>
2023-09-18 16:58:50 -0400

    Navbar basic styles

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,7 +1,20 @@
+import '../styles/navbar.css';
+import { Link } from 'react-router-dom';
+
 function Navbar() {
     return (
         <>
-            <h1>Navbar</h1>
+            <div id='navWrapper'>
+                <Link to='/' id='logoLink'>
+                    <div id='logoWrapper'>
+                        <h1 id='title'>surfConnect</h1>
+                    </div>
+                </Link>
+                <div id='navLinksWrapper'>
+                    <Link to='/login' className='navLink'>Login</Link>
+                    <Link to='/signup' className='navLink'>Sign Up</Link>
+                </div>
+            </div>
         </>
     )
 }
