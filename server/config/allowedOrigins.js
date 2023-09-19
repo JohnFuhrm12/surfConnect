91e9324f129cab5b7b26f1c27f70482b5dc7a05b
JohnFuhrm12 <61069716+JohnFuhrm12@users.noreply.github.com>
2023-09-19 17:03:58 -0400

    Configured CORS Options

---
+++ server/config/allowedOrigins.js
@@ -0,0 +1,8 @@
+const allowedOrigins = [
+    'http://127.0.0.1:5173',
+    'http://localhost:3000',
+    'https://www.surfconnect.com',
+    'https://surfconnect.com'
+];
+
+module.exports = allowedOrigins;
