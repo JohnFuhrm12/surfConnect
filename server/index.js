7355c538c348dd720ff699314ac0398ae8be3044
JohnFuhrm12 <61069716+JohnFuhrm12@users.noreply.github.com>
2023-09-19 15:37:18 -0400

    Added html for root and 404 errors on API

--- server/index.js
+++ server/index.js
@@ -17,14 +17,26 @@ db.once('open', () => {
     console.log('Connected');
 });
 
-app.get('/', (req, res) => {
-    res.render('index.ejs');
-});
-
 app.use(express.json());
+
+app.use('/', express.static(path.join(__dirname, '/public')));
+
+const rootRouter = require('./routes/root');
+app.use('/', rootRouter);
 
 const usersRouter = require('./routes/users');
 app.use('/users', usersRouter);
+
+app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json( {message: '404 Not Found'} );
+    } else {
+        res.type('txt').send('404 Not Found');
+    }
+});
 
 app.listen(PORT, () => {
     console.log('Port 3000 Connected');
