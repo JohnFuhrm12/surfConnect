1e04111a0fce3b4cd1307450588179acc29275f3
JohnFuhrm12 <61069716+JohnFuhrm12@users.noreply.github.com>
2023-12-01 18:17:36 -0300

    Profile Dashboard

--- client/src/components/Profile.tsx
+++ client/src/components/Profile.tsx
@@ -7,9 +7,20 @@ function Profile( {...props} ) {
             {props.loggedIn? 
             <>
                 <h1>Dashboard</h1>
-                <h2>Welcome back {props.authedUsername}!</h2> 
-                <h2>Role: {props.role}!</h2> 
-                <h2>Name: {props.firstName} {props.lastName}</h2> 
+                <h2>Welcome back {props.firstName}!</h2> 
+                <div id='dashboardContainer'>
+                    <div id='profileInfo'>
+                        <img id='profilePicture' src='https://res.cloudinary.com/dvmw658s9/image/upload/v1701463466/surfConnect/hhfnik5aghesyxzb25fe.png' alt='Profile'/>
+                        <h2 className='profileHeader'>{props.firstName} {props.lastName}</h2> 
+                        <h2 className='profileHeader' id='roleTitle'>{props.role}</h2>
+                    </div>
+                    <div id='purchasesContainer'>
+                        <h2 className='profileHeader'>Recent Purchases</h2>
+                    </div>
+                    <div id='editsContainer'>
+                        <button className='profileEditButton'>Edit Profile</button>
+                    </div>
+                </div>
             </>
             : 
             <Login {...props} />}
